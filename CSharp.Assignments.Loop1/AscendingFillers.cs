ae5679dd56cb6a6a71012e6d640a9cc9edc6325d
Roshan Bharati <bharatir8396@uhcl.edu>
2020-09-26 11:52:10 -0400

    hw2

--- CSharp.Assignments.Loop1/AscendingFillers.cs
+++ CSharp.Assignments.Loop1/AscendingFillers.cs
@@ -7,10 +7,18 @@ using System.Threading.Tasks;
 namespace CSharp.Assignments.Loop1
 {
     /// <summary>
-    /// Create an application that reads a bunch of non-negative integers line-by-line until a negative integer or the end-of-line (CTRL-Z) has reached. The app will print out the sum of all the fillers that will make all the values entered so far will appear in increasing order, where each value is larger than the previous number. That is each filler is added to each input, such that the numbers will all appear in increasing order (e.g. 1, 2, 3, 10). The only exception is when the element is 0, where the current number and the subsequent numbers will restart from zero.
+    /// Create an application that reads a bunch of non-negative integers 
+    ///line-by-line until a negative integer or the end-of-line (CTRL-Z) has reached. 
+    ///The app will print out the sum of all the fillers that will make all the values entered so far will appear 
+    ///in increasing order, where each value is larger than the previous number. 
+    ///That is each filler is added to each input, such that the 
+    ///numbers will all appear in increasing order (e.g. 1, 2, 3, 10). 
+    ///The only exception is when the element is 0, where the current number and 
+    ///the subsequent numbers will restart from zero.
     /// </summary>
     /// <param name="a"></param>
-    /// <remarks>In essence, this app will calculate the sum of all these values added to the original numbers such that our projected numbers will appear in increasing order.</remarks>
+    /// <remarks>In essence, this app will calculate the sum of all these values added to 
+    /// the original numbers such that our projected numbers will appear in increasing order.</remarks>
     /// <returns>The sum of all the fillers</returns>
     public class AscendingFillers
     {
